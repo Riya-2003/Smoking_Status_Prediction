f6ea961495cc71d4e06e4dd40ea53cc03b279da4
Riya Jain <92114998+Riya-2003@users.noreply.github.com>
2023-04-23 23:40:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -38,9 +38,9 @@ def predict():
     
     st.success('The Prediction is : ' + str(label))
     if prediction[0] == 1:
-        st.success['The person is a smoker :smoking:']
+        st.write('The person is a smoker :smoking:')
         
     else:
-        st.success['The person is a non-smoker :no_smoking:']
+        st.write('The person is a non-smoker :no_smoking:')
     
 trigger = st.button('Predict', on_click=predict)
